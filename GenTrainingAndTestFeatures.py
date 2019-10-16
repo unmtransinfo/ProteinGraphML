c5c0ee5511b4d348c8c90d657f1584f3356b919f
Praveen Kumar <praveen.nitsurat@gmail.com>
2019-10-16 12:46:06 -0600

    modifiy argparse

--- GenTrainingAndTestFeatures.py
+++ GenTrainingAndTestFeatures.py
@@ -21,7 +21,7 @@ def savePickleObject(fileName, data):
 	with open(fileName, 'wb') as handle:
 		pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-def saveTrainTestSet(allData):
+def saveTrainTestSet(allData, outputDir, disease=None, trainingfile=None, testfile=None):
 	'''
 	This function saves training and test in pickle format 
 	'''
@@ -73,114 +73,115 @@ def saveTrainTestSet(allData):
 	
  
 ############### START OF THE CODE ##############################
-t0 = time.time()
-
-#DATA_DIR = os.getcwd() + '/DataForML/'
-DEFAULT_GRAPH = "ProteinDisease_GRAPH.pkl"
-DEFAULT_STATIC_FEATURES = "gtex,lincs,ccle,hpa"
-
-parser = argparse.ArgumentParser(description='Generate features for training and test set', epilog='Protein Ids with True label must be provided')
-parser.add_argument('--disease', metavar='disease', help='Mammalian Phenotype ID, e.g. MP_0000180')
-parser.add_argument('--trainingfile', help='pickled training set, e.g. "diabetes.pkl"')
-parser.add_argument('--testfile', help='pickled test set, e.g. "diabetes_test.pkl"')
-parser.add_argument('--outputdir', required=True, help='directory where train and test data with features will be saved, e.g. "diabetes_no_lincs"')
-parser.add_argument('--kgfile', default=DEFAULT_GRAPH, help='input pickled KG (default: "{0}")'.format(DEFAULT_GRAPH))
-parser.add_argument('--static_data', default=DEFAULT_STATIC_FEATURES, help='(default: "{0}")'.format(DEFAULT_STATIC_FEATURES))
-parser.add_argument("-v", "--verbose", action="count", default=0, help="verbosity")
-
-argData = vars(parser.parse_args())
-
-logging.basicConfig(format='%(levelname)s:%(message)s', level=(logging.DEBUG if argData['verbose']>1 else logging.INFO))
-
-#Get data from file or disease
-disease = argData['disease']
-trainingfile = argData['trainingfile']
-testfile = argData['testfile']
-fileData = None
-
-#folder where train and test data with features will be stored
-outputDir = argData['outputdir']
-if not os.path.isdir(outputDir):
-	logging.info('Create the output directory')
-	os.makedirs(outputDir)
-logging.info('Output directory for ML data(Training/Test): {0}'.format(outputDir))
-
-#check whether file or disease was given
-if (trainingfile is None and disease is None): 
-	parser.error("--disease or -- training file must be specified.")
-
-#fetch KG data
-graphString = argData['kgfile']
-currentGraph = ProteinDiseaseAssociationGraph.load(graphString)
-logging.info("GRAPH {0} LOADED".format(graphString))
-
-#Access the adapter
-dbAdapter = OlegDB()
-
-if (trainingfile is not None and disease is None): 
-	trainingPklFile = trainingfile
-	logging.info('Input training file: {0}'.format(trainingPklFile))
-	try:
-		with open(trainingPklFile, 'rb') as f:
-			fileData = pickle.load(f)
-	except:
-		logging.error('Invalid pickled training set file') 
-		exit()
-
-	#Also add test data if provided
-	if (testfile is not None):
-		testPklFile = testfile
-		logging.info('Input test file: {0}'.format(testPklFile))
-		try:
-			with open(testPklFile, 'rb') as f:
-				fileData.update(pickle.load(f)) #fileData will now have both train and test set
-		except:
-			logging.error('Invalid pickled test set file') 
-			exit()	    		
-elif (trainingfile is None and disease is not None): 
-	logging.info("running on this disease: {0}".format(disease))
-	fullData = {}
-	#get positive and negative training protein ids
-	trainP, trainF = getTrainingProteinIds(disease,currentGraph) 
-	fullData[True] = trainP
-	fullData[False] = trainF
-	#get all protein ids
-	allProteinIds = dbAdapter.fetchAllProteinIds()
-	allProteinIds = set(allProteinIds['protein_id'].tolist())
-	#prepare test set
-	testProteinSet = allProteinIds.difference(trainP)
-	testProteinSet = testProteinSet.difference(trainF)
-	fullData['unknown'] = testProteinSet
-	fileData = fullData
-else:
-	logging.error('Wrong parameters passed')	
-
-
-#Nodes
-nodes = [ProteinInteractionNode,KeggNode,ReactomeNode,GoNode,InterproNode]
-
-#Static features that need to be included
-staticFeatures = argData['static_data'].split(',')
-#staticFeatures = []
-logging.info(staticFeatures)
-logging.info("--- USING {0} METAPATH FEATURE SETS".format(len(nodes)))
-logging.info("--- USING {0} STATIC FEATURE SETS".format(len(staticFeatures)))
-
-#fetch the description of proteins
-idDescription = dbAdapter.fetchPathwayIdDescription() #fetch the description
-
-#generate features
-if fileData is not None:
-	#logging.info("FOUND {0} POSITIVE LABELS".format(len(fileData[True])))
-	#logging.info("FOUND {0} NEGATIVE LABELS".format(len(fileData[False])))
-	allData = metapathFeatures(disease,currentGraph,nodes,idDescription,staticFeatures,loadedLists=fileData).fillna(0) 
-else:
-	#allData = metapathFeatures(disease,currentGraph,nodes,idDescription,staticFeatures).fillna(0)
-	logging.error('fileData should not be None')
-	exit()
+if __name__ == '__main__':
+	t0 = time.time()
+	#DATA_DIR = os.getcwd() + '/DataForML/'
+	DEFAULT_GRAPH = "ProteinDisease_GRAPH.pkl"
+	DEFAULT_STATIC_FEATURES = "gtex,lincs,ccle,hpa"
 	
+	#Command-line arguments
+	parser = argparse.ArgumentParser(description='Generate features for training and test set', epilog='Protein Ids with True label must be provided')
+	parser.add_argument('--disease', metavar='disease', help='Mammalian Phenotype ID, e.g. MP_0000180')
+	parser.add_argument('--trainingfile', help='pickled training set, e.g. "diabetes.pkl"')
+	parser.add_argument('--testfile', help='pickled test set, e.g. "diabetes_test.pkl"')
+	parser.add_argument('--outputdir', required=True, help='directory where train and test data with features will be saved, e.g. "diabetes_no_lincs"')
+	parser.add_argument('--kgfile', default=DEFAULT_GRAPH, help='input pickled KG (default: "{0}")'.format(DEFAULT_GRAPH))
+	parser.add_argument('--static_data', default=DEFAULT_STATIC_FEATURES, help='(default: "{0}")'.format(DEFAULT_STATIC_FEATURES))
+	parser.add_argument("-v", "--verbose", action="count", default=0, help="verbosity")
 
-#Divide allData into training/test set and save them
-saveTrainTestSet(allData)
+	#argData = vars(parser.parse_args())
+	args = parser.parse_args()
+	logging.basicConfig(format='%(levelname)s:%(message)s', level=(logging.DEBUG if args.verbose>1 else logging.INFO))
 
-logging.info('{0}: elapsed time: {1}'.format(os.path.basename(sys.argv[0]), time.strftime('%Hh:%Mm:%Ss', time.gmtime(time.time()-t0))))
+	#Get data from file or disease
+	#disease = argData['disease']
+	#trainingfile = argData['trainingfile']
+	#testfile = argData['testfile']
+	fileData = None
+
+	#folder where train and test data with features will be stored
+	#outputDir = argData['outputdir']
+	if not os.path.isdir(args.outputdir):
+		logging.info('Create the output directory')
+		os.makedirs(args.outputdir)
+	logging.info('Output directory for ML data(Training/Test): {0}'.format(args.outputdir))
+
+	#check whether file or disease was given
+	if (args.trainingfile is None and args.disease is None): 
+		parser.error("--disease or -- training file must be specified.")
+
+	#fetch KG data
+	#graphString = args.kgfile
+	currentGraph = ProteinDiseaseAssociationGraph.load(args.kgfile)
+	logging.info("GRAPH {0} LOADED".format(args.kgfile))
+
+	#Access the adapter
+	dbAdapter = OlegDB()
+
+	if (args.trainingfile is not None and args.disease is None): 
+		#trainingPklFile = trainingfile
+		logging.info('Input training file: {0}'.format(args.trainingfile))
+		try:
+			with open(args.trainingfile, 'rb') as f:
+				fileData = pickle.load(f)
+		except:
+			logging.error('Invalid pickled training set file') 
+			exit()
+
+		#Also add test data if provided
+		if (args.testfile is not None):
+			#testPklFile = testfile
+			logging.info('Input test file: {0}'.format(args.testfile))
+			try:
+				with open(args.testfile, 'rb') as f:
+					fileData.update(pickle.load(f)) #fileData will now have both train and test set
+			except:
+				logging.error('Invalid pickled test set file') 
+				exit()	    		
+	elif (args.trainingfile is None and args.disease is not None): 
+		logging.info("running on this disease: {0}".format(args.disease))
+		fullData = {}
+		#get positive and negative training protein ids
+		trainP, trainF = getTrainingProteinIds(args.disease,currentGraph) 
+		fullData[True] = trainP
+		fullData[False] = trainF
+		#get all protein ids
+		allProteinIds = dbAdapter.fetchAllProteinIds()
+		allProteinIds = set(allProteinIds['protein_id'].tolist())
+		#prepare test set
+		testProteinSet = allProteinIds.difference(trainP)
+		testProteinSet = testProteinSet.difference(trainF)
+		fullData['unknown'] = testProteinSet
+		fileData = fullData
+	else:
+		logging.error('Wrong parameters passed')	
+
+
+	#Nodes
+	nodes = [ProteinInteractionNode,KeggNode,ReactomeNode,GoNode,InterproNode]
+
+	#Static features that need to be included
+	staticFeatures = args.static_data.split(',')
+	#staticFeatures = []
+	logging.info(staticFeatures)
+	logging.info("--- USING {0} METAPATH FEATURE SETS".format(len(nodes)))
+	logging.info("--- USING {0} STATIC FEATURE SETS".format(len(staticFeatures)))
+
+	#fetch the description of proteins
+	idDescription = dbAdapter.fetchPathwayIdDescription() #fetch the description
+
+	#generate features
+	if fileData is not None:
+		#logging.info("FOUND {0} POSITIVE LABELS".format(len(fileData[True])))
+		#logging.info("FOUND {0} NEGATIVE LABELS".format(len(fileData[False])))
+		allData = metapathFeatures(args.disease,currentGraph,nodes,idDescription,staticFeatures,loadedLists=fileData).fillna(0) 
+	else:
+		#allData = metapathFeatures(args.disease,currentGraph,nodes,idDescription,staticFeatures).fillna(0)
+		logging.error('fileData should not be None')
+		exit()
+		
+
+	#Divide allData into training/test set and save them
+	saveTrainTestSet(allData, args.outputdir, args.disease, args.trainingfile, args.testfile)
+
+	logging.info('{0}: elapsed time: {1}'.format(os.path.basename(sys.argv[0]), time.strftime('%Hh:%Mm:%Ss', time.gmtime(time.time()-t0))))
