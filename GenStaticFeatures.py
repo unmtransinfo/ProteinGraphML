6844c8b017b27b92e2c8ed654a8621ad5e86d5a2
Jeremy J Yang <jeremyjyang@gmail.com>
2019-12-12 08:35:39 -0700

    Ok.

--- GenStaticFeatures.py
+++ GenStaticFeatures.py
@@ -15,9 +15,17 @@ if __name__ == '__main__':
   dbad = OlegDB()
 
   gtex = staticData.gtex(dbad)
-  logging.info("rows: {0}; cols: {1}".format(gtex.shape[0], gtex.shape[1]))
+  logging.info("GTEX: rows: {0}; cols: {1}".format(gtex.shape[0], gtex.shape[1]))
   gtex.to_csv("gtex.tsv", "\t", index=False)
 
+  hpa = staticData.hpa(dbad)
+  logging.info("HPA: rows: {0}; cols: {1}".format(hpa.shape[0], hpa.shape[1]))
+  hpa.to_csv("hpa.tsv", "\t", index=False)
+
+  ccle = staticData.ccle(dbad)
+  logging.info("CCLE: rows: {0}; cols: {1}".format(ccle.shape[0], ccle.shape[1]))
+  ccle.to_csv("ccle.tsv", "\t", index=False)
+
   lincs = staticData.lincs(dbad)
-  logging.info("rows: {0}; cols: {1}".format(lincs.shape[0], lincs.shape[1]))
+  logging.info("LINCS: rows: {0}; cols: {1}".format(lincs.shape[0], lincs.shape[1]))
   lincs.to_csv("lincs.tsv", "\t", index=False)
