e9c1836e9fbd3c6c5616886ebc781fd24d9a7db3
Jeremy Yang <jeremyjyang@gmail.com>
2019-12-12 11:02:20 -0700

    Working ok.

--- GenStaticFeatures.py
+++ GenStaticFeatures.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 ###
 
-import sys,os,argparse,time
+import sys,os,argparse,re,time
 import logging
 import pandas as pd
 
@@ -10,22 +10,56 @@ from ProteinGraphML.MLTools.StaticFeatures import staticData
 
 ###
 if __name__ == '__main__':
+  SOURCES = ["gtex", "lincs", "ccle", "hpa"]
+  parser = argparse.ArgumentParser(description='Generate static features for all proteins.')
+  parser.add_argument('--outputdir', default='.')
+  parser.add_argument('--sources', type=str, help="comma-separated list: %s"%(','.join(SOURCES)), default=(','.join(SOURCES)))
+  parser.add_argument('--decimals', type=int, default=3) 
+  parser.add_argument("-v", "--verbose", action="count", default=0)
+  args = parser.parse_args()
+
+  logging.basicConfig(format='%(levelname)s:%(message)s', level=(logging.DEBUG if args.verbose>1 else logging.INFO))
+
+  if not args.sources:
+    parser.error("--sources required.")
+
+  sources = re.split("[, ]", args.sources.strip())
+  if len(set(sources) - set(SOURCES))>0:
+    parser.error("Invalid sources: %s"%(','.join(list(set(sources) - set(SOURCES)))))
 
   t0 = time.time()
   dbad = OlegDB()
 
-  gtex = staticData.gtex(dbad)
-  logging.info("GTEX: rows: {0}; cols: {1}".format(gtex.shape[0], gtex.shape[1]))
-  gtex.to_csv("gtex.tsv", "\t", index=False)
+  if "gtex" in sources:
+    ofile_gtex = args.outputdir+"/gtex.tsv"
+    logging.info("GTEX: writing {0}".format(ofile_gtex))
+    gtex = staticData.gtex(dbad)
+    logging.info("GTEX: rows: {0}; cols: {1}".format(gtex.shape[0], gtex.shape[1]))
+    gtex.round(args.decimals).to_csv(ofile_gtex, "\t", index=True)
+    logging.info('{0}: elapsed time: {1}'.format(os.path.basename(sys.argv[0]), time.strftime('%Hh:%Mm:%Ss', time.gmtime(time.time()-t0))))
 
-  hpa = staticData.hpa(dbad)
-  logging.info("HPA: rows: {0}; cols: {1}".format(hpa.shape[0], hpa.shape[1]))
-  hpa.to_csv("hpa.tsv", "\t", index=False)
+  if "hpa" in sources:
+    ofile_hpa = args.outputdir+"/hpa.tsv"
+    logging.info("HPA: writing {0}".format(ofile_hpa))
+    hpa = staticData.hpa(dbad)
+    logging.info("HPA: rows: {0}; cols: {1}".format(hpa.shape[0], hpa.shape[1]))
+    hpa.round(args.decimals).to_csv(ofile_hpa, "\t", index=True)
+    logging.info('{0}: elapsed time: {1}'.format(os.path.basename(sys.argv[0]), time.strftime('%Hh:%Mm:%Ss', time.gmtime(time.time()-t0))))
 
-  ccle = staticData.ccle(dbad)
-  logging.info("CCLE: rows: {0}; cols: {1}".format(ccle.shape[0], ccle.shape[1]))
-  ccle.to_csv("ccle.tsv", "\t", index=False)
+  if "lincs" in sources:
+    ofile_lincs = args.outputdir+"/lincs.tsv"
+    logging.info("LINCS: writing {0}".format(ofile_lincs))
+    lincs = staticData.lincs(dbad)
+    logging.info("LINCS: rows: {0}; cols: {1}".format(lincs.shape[0], lincs.shape[1]))
+    lincs.round(args.decimals).to_csv(ofile_lincs, "\t", index=True)
 
-  lincs = staticData.lincs(dbad)
-  logging.info("LINCS: rows: {0}; cols: {1}".format(lincs.shape[0], lincs.shape[1]))
-  lincs.to_csv("lincs.tsv", "\t", index=False)
+  if "ccle" in sources:
+    ofile_ccle = args.outputdir+"/ccle.tsv"
+    logging.info("CCLE: writing {0}".format(ofile_ccle))
+    ccle = staticData.ccle(dbad)
+    logging.info("CCLE: rows: {0}; cols: {1}".format(ccle.shape[0], ccle.shape[1]))
+    ccle.round(args.decimals).to_csv(ofile_ccle, "\t", index=True)
+    logging.info('{0}: elapsed time: {1}'.format(os.path.basename(sys.argv[0]), time.strftime('%Hh:%Mm:%Ss', time.gmtime(time.time()-t0))))
+
+  logging.info('{0}: elapsed time: {1}'.format(os.path.basename(sys.argv[0]), time.strftime('%Hh:%Mm:%Ss', time.gmtime(time.time()-t0))))
+
--- ProteinGraphML/MLTools/StaticFeatures/staticData.py
+++ ProteinGraphML/MLTools/StaticFeatures/staticData.py
@@ -1,4 +1,5 @@
 import logging
+import pandas as pd
 import numpy as np
 
 def basicPivot(df, key, column, value):
@@ -8,6 +9,7 @@ def gtex(DBadapter):
   df = DBadapter.loadGTEX()
   logging.info("staticData: GTEX DBadapter: rows: {0}; cols: {1}".format(df.shape[0], df.shape[1]))
   df = basicPivot(df, "protein_id", "tissue_type_detail", "median_tpm")
+  df.reset_index(drop=False, inplace=True)
   logging.info("staticData: GTEX proteins: rows: {0}; cols: {1}".format(df.shape[0], df.shape[1]))
   return df
 
@@ -15,6 +17,7 @@ def lincs(DBadapter):
   df = DBadapter.loadLINCS()
   logging.info("staticData: LINCS DBadapter: rows: {0}; cols: {1}".format(df.shape[0], df.shape[1]))
   df = basicPivot(df, "protein_id", "col_id", "zscore")
+  df.reset_index(drop=False, inplace=True)
   logging.info("staticData: LINCS proteins: rows: {0}; cols: {1}".format(df.shape[0], df.shape[1]))
   return df
 
@@ -22,8 +25,10 @@ def ccle(DBadapter):
   df = DBadapter.loadCCLE()
   logging.info("staticData: CCLE DBadapter: rows: {0}; cols: {1}".format(df.shape[0], df.shape[1]))
   df["col_id"] = df.cell_id
-  df.loc[df.tissue.notna(), "col_id"] = (df.cell_id.str+"_"+df.tissue.str)
+  df.loc[df.tissue.notna(), "col_id"] = (df.cell_id+"_"+df.tissue)
   df = df[["protein_id", "col_id", "expression"]].drop_duplicates()
+  df = basicPivot(df, "protein_id", "col_id", "expression")
+  df.reset_index(drop=False, inplace=True)
   logging.info("staticData: CCLE proteins: rows: {0}; cols: {1}".format(df.shape[0], df.shape[1]))
   return df
 
@@ -31,6 +36,8 @@ def hpa(DBadapter):
   df = DBadapter.loadHPA()
   logging.info("staticData: HPA DBadapter: rows: {0}; cols: {1}".format(df.shape[0], df.shape[1]))
 
+  #R:
+  #(Why did Oleg use mode not median?)
   #hpa$level <- factor(hpa$level, levels = c("not detected", "low", "medium", "high"), ordered=F)
   #hpa <- unique(hpa)
   #hpa[, col_id := gsub(" ", "_", col_id, fixed = T)]
@@ -42,11 +49,19 @@ def hpa(DBadapter):
   #hpa.sparse.matrix <- sparse.model.matrix(~.-1, data = hpa)
   #hpa <- as.data.table(as.matrix(hpa.sparse.matrix), keep.rownames = F)
 
-  hpa = hpa.drop_duplicates()
-  hpa.col_id = hpa.col_id.str.replace(",", "", regex=False)
-  hpa.col_id = hpa.col_id.str.replace("[ /-]", "_")
+  df = df.drop_duplicates()
+  df.col_id = df.col_id.str.replace(",", "", regex=False)
+  df.col_id = df.col_id.str.replace("[ /-]", "_")
+  df = df.rename(columns={'level':'level_str'})
+  df["level"] = pd.Series(dtype=int)
+  df.loc[df.level_str == "not detected", "level"] = 0
+  df.loc[df.level_str == "low", "level"] = 1
+  df.loc[df.level_str == "medium", "level"] = 2
+  df.loc[df.level_str == "high", "level"] = 3
+  df.loc[df.level_str.isna(), "level"] = 0
   df = basicPivot(df, "protein_id", "col_id", "level")
-  df.loc[df.level.isna(), "level"] = "not detected"
+  df.fillna(0)
+  df.reset_index(drop=False, inplace=True)
   logging.info("staticData: HPA proteins: rows: {0}; cols: {1}".format(df.shape[0], df.shape[1]))
   return df
 
