0955bc5bc08cea1462e3f89dcf329c136b858342
Jeremy Yang <jeremyjyang@gmail.com>
2020-05-11 09:58:54 -0600

    Ok.

--- sql/tcrd_mousephenotype.sql
+++ sql/tcrd_mousephenotype.sql
@@ -2,16 +2,13 @@
 SELECT 
         COUNT(DISTINCT ph.nhprotein_id) AS mouse_protein_ids, 
         COUNT(DISTINCT ph.term_id) AS mp_term_ids, 
-        COUNT(DISTINCT mpo.name) AS mp_term_names, 
+        COUNT(DISTINCT ph.term_name) AS mp_term_names, 
         COUNT(DISTINCT ph.procedure_name) AS mp_procedure_names, 
         COUNT(DISTINCT ph.parameter_name) AS mp_param_names, 
         ph.gp_assoc AS association 
 FROM
-        phenotype ph,
-        mpo
+        phenotype ph
 WHERE
-        ph.term_id = mpo.mpid
-        AND
         ptype = 'IMPC'
 GROUP BY
         association
--- sql/tcrd_mousephenotype2.sql
+++ sql/tcrd_mousephenotype2.sql
@@ -1,21 +1,15 @@
-SELECT DISTINCT
+SELECT
         ph.nhprotein_id AS protein_id_m,
         ph.term_id AS mp_term_id,
-        mpo.name AS mp_term_name,
+        ph.term_name AS mp_term_name,
         ph.p_value,
         ph.effect_size,
         ph.procedure_name,
         ph.parameter_name,
         ph.gp_assoc AS association
 FROM
-        phenotype ph,
-        mpo
+        phenotype ph
 WHERE
-        ph.term_id = mpo.mpid
-        AND
         ph.ptype = 'IMPC'
         ;
---        AND 
---        ph.term_id IN ('MP:0000180', 'MP:0005177', 'MP:0001549', 'MP:0005559')
---        ph.term_id IN ('MP:0000180', 'MP:0005177', 'MP:0001549')
---        ph.term_id = 'MP:0003947'
+
