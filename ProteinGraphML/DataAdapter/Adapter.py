01420d4add36f24695ecded038e318ac5e0a0cc5
Jeremy Yang <jeremyjyang@gmail.com>
2020-08-13 08:36:09 -0600

    TCRD default negatives well defined.

--- ProteinGraphML/DataAdapter/Adapter.py
+++ ProteinGraphML/DataAdapter/Adapter.py
@@ -750,16 +750,21 @@ WHERE
     def fetchNegativeClassProteinIds(self):
         sql = """\
 SELECT DISTINCT
-    clinvar.protein_id
+	clinvar.protein_id,
+	p.sym,
+	p.uniprot,
+	p.description
 FROM
-    clinvar
+	clinvar
 JOIN
-    clinvar_phenotype ON clinvar.clinvar_phenotype_id = clinvar_phenotype.id
+	protein p ON p.id = clinvar.protein_id
 JOIN
-    clinvar_phenotype_xref ON clinvar_phenotype.id = clinvar_phenotype_xref.clinvar_phenotype_id
+	clinvar_phenotype ON clinvar.clinvar_phenotype_id = clinvar_phenotype.id
+JOIN
+	clinvar_phenotype_xref ON clinvar_phenotype.id = clinvar_phenotype_xref.clinvar_phenotype_id
 WHERE 
-    clinvar_phenotype_xref.source = 'OMIM'
-    AND clinvar.clinical_significance != 'Uncertain significance'
+	clinvar_phenotype_xref.source = 'OMIM'
+	;
 """
         negProteinIds = selectAsDF(sql, ['protein_id'], self.db)
         logging.debug(
--- sql/tcrd_default_negatives.sql
+++ sql/tcrd_default_negatives.sql
@@ -1,13 +1,16 @@
--- From DataAdaptor/Adapter.py
 SELECT DISTINCT
-	clinvar.protein_id
+	clinvar.protein_id,
+	p.sym,
+	p.uniprot,
+	p.description
 FROM
 	clinvar
+JOIN
+	protein p ON p.id = clinvar.protein_id
 JOIN
 	clinvar_phenotype ON clinvar.clinvar_phenotype_id = clinvar_phenotype.id
 JOIN
 	clinvar_phenotype_xref ON clinvar_phenotype.id = clinvar_phenotype_xref.clinvar_phenotype_id
 WHERE 
 	clinvar_phenotype_xref.source = 'OMIM'
-	AND clinvar.clinical_significance != 'Uncertain significance'
 	;
