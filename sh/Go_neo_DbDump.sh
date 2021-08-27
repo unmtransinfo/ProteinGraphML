f57ea1f4ab8a31238a0be8b271bba17b5e9c3e7d
Jeremy Yang <jeremyjyang@gmail.com>
2021-08-27 13:46:40 -0600

    Alright.

--- sh/Go_neo_DbDump.sh
+++ sh/Go_neo_DbDump.sh
@@ -16,5 +16,20 @@ sudo -u neo4j neo4j-admin dump --database=${DBNAME} --to=$DATADIR/mpml_neo4j_${D
 sudo systemctl -l start neo4j
 sudo systemctl -l status neo4j
 #
+###
+# Neo4j Server: 
 #sudo -u neo4j neo4j-admin load --database=${DBNAME} --from=$DATADIR/mpml_neo4j_${DBNAME}.dump
 #[--force]
+###
+# Neo4j Desktop: 
+# To restore this graph database from the dump file:
+#   - Install and launch the Neo4j Desktop Client.
+#   - Create a new database (do not start the database).
+#   - Click on the ... then click Manage in the pop up menu, then click Open Terminal.
+#   - Load the dump file with the following command, changing PATHTOFILE as needed.
+#
+# bin/neo4j-admin load --from=PATHTOFILE/mpml.dump --database=neo4j
+#
+#   - Exit terminal and click Start to start the database.
+#   - Click Open to use built in Neo4j browser to query and explore the database.
+###
